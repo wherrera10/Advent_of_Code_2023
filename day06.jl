5895ec12bfd92d81e6c9291154716ba78eabbcea
Bill <wherrera10@users.noreply.github.com>
2023-12-05 23:52:40 -1000

    Update day06.jl

--- day06.jl
+++ day06.jl
@@ -3,21 +3,17 @@ let
     part = [0, 0]
 
     lines = split(read("day06.txt", String), "\n")
-
     times, distances = [[parse(Int, x) for x in split(lines[i])[2:end]] for i in 1:2]
 
     part1bests(t, d) = count((t - wait) * wait > d for wait in 1:t)
 
     part[1] = prod([part1bests(times[r], distances[r]) for r in eachindex(times)])
 
-
     bigtime, bigdist = [parse(Int, prod(split(lines[i])[2:end])) for i in 1:2]
-
     begintime = findfirst(w -> (bigtime - w) * w > bigdist, 1:bigtime)
-    endtime = findlast(w -> (bigtime - w) * w > bigdist, 1:bigtime)
 
-    part[2] = endtime - begintime + 1
+    part[2] = bigtime - begintime - begintime + 1
 
-    @show part[1], part[2]
+    @show part[1], part[2] # (440000, 26187338)
 
 end
