8ed590bcdf945db9984b1b9f4026ce2b7dd183a9
Bill <wherrera10@users.noreply.github.com>
2023-12-07 06:44:19 -1000

    Update day07.jl

--- day07.jl
+++ day07.jl
@@ -1,35 +1,28 @@
 const part = [0, 0]
-const values = Dict{Char, Int}(c => i for (i, c) in enumerate(collect("23456789TJQKA")))
-const newvalues = Dict{Char, Int}(c => i for (i, c) in enumerate(collect("J23456789TQKA")))
 
-function score(hand, oldhand = Int[])
-    most2, most = sort!([count(==(x), hand) for x in "23456789TJQKA"])[end-1:end]
-    (most == 5 ? 6 : most == 4 ? 5 : most == 3 ? (most2 > 1 ? 4 : 3) :
-     most == 2 ? (most2 == 2 ? 2 : 1) : 0),
-    (isempty(oldhand) ? map(x -> values[x], hand) : map(x -> newvalues[x], oldhand))
-end
-
-const entries = filter(!isempty, split(read("day07.txt", String)))
-const hands = [collect(h) for (i, h) in enumerate(entries) if isodd(i)]
+const txt = read("day07.txt", String)
+const entries = split(replace(txt, "T" => "a", "J" => "b", "Q" => "c", "K" => "d", "A" => "e"))
+const hands = [h for (i, h) in enumerate(entries) if isodd(i)]
 const bids = [parse(Int, h) for (i, h) in enumerate(entries) if iseven(i)]
-const zipped = collect(zip(hands, bids, map(score, hands)))
-part[1] = sum(x[2] * i for (i, x) in enumerate(sort(zipped; by = last)))
+const wildhands = map(s -> replace(s, "b" => "1"), hands)
 
-function usejoker(hand)
-    jloc = findall(==('J'), hand)
-    jcount = length(jloc)
-    jcount == 0 && return hand
-    jcount == 5 && return fill('A', 5)
-    noj = filter(!=('J'), hand)
-    c = score(noj)[1] in [0, 2] ? sort(noj, by = x -> newvalues[x])[end] :
-        first(filter(x -> count(==(x), noj) >= 2, noj))
-    newhand = deepcopy(hand)
-    newhand[jloc] .= c
-    return newhand
+function score(hand)
+    most2, most = sort!([count(==(x), hand) for x in "123456789abcde"])[end-1:end]
+    return most * 10 + most2
 end
 
-const wilds = map(usejoker, hands)
-const newzipped = collect(zip(wilds, bids, [score(wilds[i], hands[i]) for i in eachindex(hands)]))
-part[2] = sum(x[2] * i for (i, x) in enumerate(sort!(newzipped; by = last)))
+function wildscore(sco, hand)
+    numj = count(==('b'), hand)
+    return numj == 5 ? 50 : numj == 4 ? 50 : numj == 3 ? (sco == 32 ? 50 : 41) :
+           numj == 2 ? (sco == 32 ? 50 : sco == 22 ? 41 : 31) :
+           (sco == 41 ? 50 : sco == 31 ? 41 : sco == 22 ? 32 : sco == 21 ? 31 : 21)
+end
+
+const scores = map(score, hands)
+const wildscores = map(i -> wildscore(scores[i], hands[i]), eachindex(scores))
+
+part[1] = sum(x[3] * i for (i, x) in enumerate(sort!(collect(zip(scores, hands, bids)))))
+
+part[2] = sum(x[3] * i for (i, x) in enumerate(sort!(collect(zip(wildscores, wildhands, bids)))))
 
 @show part[1], part[2] # (part[1], part[2]) = (248113761, 246285222)
