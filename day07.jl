15b408a9a3ff45ee410a0ff479fb2e9bf5c65801
Bill <wherrera10@users.noreply.github.com>
2023-12-06 23:03:28 -1000

    Update day07.jl

--- day07.jl
+++ day07.jl
@@ -13,6 +13,7 @@ let
         ca != cb && return ca < cb
         return [values[x] for x in a] < [values[x] for x in b]
     end
+
     function lt_hands(a, b, a1, b1)
         ca, cb = score(a), score(b)
         ca != cb && return ca < cb
@@ -22,8 +23,8 @@ let
     entries = filter(!isempty, split(read("day07.txt", String)))
     hands = [collect(h) for (i, h) in enumerate(entries) if isodd(i)]
     bids = [parse(Int, h) for (i, h) in enumerate(entries) if iseven(i)]
-    ranked = sort(zip(hands, bids), lt = (x, y) -> lt_hands(x[1], y[1]))
-    part[1] = sum(x[2] * i for (i, x) in enumerate(ranked))
+    part[1] = sum(x[2] * i for (i, x) in enumerate(sort(zip(hands, bids),
+       lt = (x, y) -> lt_hands(x[1], y[1]))))
 
     function usejoker(hand)
         jloc = findall(==('J'), hand)
@@ -32,16 +33,15 @@ let
         jcount == 5 && return fill('A', 5)
         noj = filter(!=('J'), hand)
         c =  score(noj) in [0, 2] ? sort(noj, by = x -> newvalues[x])[end] :
-                                    first(filter(x -> count(==(x), noj) >= 2, noj))
+                                  first(filter(x -> count(==(x), noj) >= 2, noj))
         newhand = deepcopy(hand)
         newhand[jloc] .= c
         return newhand
     end
 
     newvalues = Dict{Char, Int}(c => i for (i, c) in enumerate(collect("J23456789TQKA")))
-    newzipped = zip(map(usejoker, hands), bids, hands)
-    newranked = sort(newzipped, lt = (x, y) -> lt_hands(x[1], y[1], x[3], y[3]))
-    part[2] = sum(x[2] * i for (i, x) in enumerate(newranked))
+    part[2] = sum(x[2] * i for (i, x) in enumerate(sort(zip(map(usejoker, hands), bids, hands),
+       lt = (x, y) -> lt_hands(x[1], y[1], x[3], y[3]))))
 
     @show part[1], part[2] # (part[1], part[2]) = (248113761, 246285222)
 end
