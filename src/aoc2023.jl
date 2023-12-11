de4f80d6448978a2d39acab196ae57dc76b82fce
Bill <wherrera10@users.noreply.github.com>
2023-12-10 21:55:28 -1000

    Update aoc2023.jl

--- src/aoc2023.jl
+++ src/aoc2023.jl
@@ -375,36 +375,30 @@ function day11()
     part = [0, 0]
     txt = filter(!=(Int32('\n')), read("day11.txt"))
     mat = reshape(txt, 140, 140)'
-    needrow, needcol = Int[], Int[]
+    nr, nc = Int[], Int[]
     for (i, r) in enumerate(eachrow(mat))
-        all(==(Int32('.')), r) && push!(needrow, i)
+        all(==(Int32('.')), r) && push!(nr, i)
     end
     for (i, c) in enumerate(eachcol(mat))
-        all(==(Int32('.')), c) && push!(needcol, i)
+        all(==(Int32('.')), c) && push!(nc, i)
     end
 
     xp = [2, 1_000_000]
-    gal = findall(==(UInt8('#')), mat)
+    gal = Tuple.(findall(==(UInt8('#')), mat))
     for k in eachindex(xp), i in 1:lastindex(gal)-1, j in i+1:lastindex(gal)
-        x1, y1 = Tuple(gal[i])
-        x2, y2 = Tuple(gal[j])
-        if x1 > x2
-            x1, x2 = x2, x1
-        end
-        if y1 > y2
-            y1, y2 = y2, y1
-        end
-        part[k] += dist(x1, y1, x2, y2) + xxp(x1, x2, needrow, xp[k]) + yxp(y1, y2, needcol, xp[k])
+        x1, y1 = gal[i]
+        x2, y2 = gal[j]
+        part[k] += dist(x1, y1, x2, y2) + xpan(x1, x2, nr, xp[k]) + xpan(y1, y2, nc, xp[k])
     end
 
     return part
 end
 
 dist(x1, y1, x2, y2) = abs(x1 - x2) + abs(y1 - y2)
-xxp(x1, x2, nr, mult) = count(i -> i ∈ nr, x1:x2-1) * (mult - 1)
-yxp(y1, y2, nc, mult) = count(i -> i ∈ nc, y1:y2-1) * (mult - 1)
+xpan(a, b, empties, xpan) = count(i -> i ∈ empties, a < b ? (a:b-1) : (b:a-1)) * (xpan - 1)
 
-day11()
+@show day11()
+
 
 
 
