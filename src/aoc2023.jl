59dbe7a717900f27bc6d57919ba47012cd06e0bd
Bill <wherrera10@users.noreply.github.com>
2023-12-10 14:59:23 -1000

    Update aoc2023.jl

--- src/aoc2023.jl
+++ src/aoc2023.jl
@@ -325,7 +325,15 @@ end
 const moves = [[0, 1], [1, 0], [0, -1], [-1, 0]]
 const charmove = Dict{Char, Vector{Int}}('L' => [1, 4], '|' => [2, 4], '-' => [1, 3], '7' => [2, 3],
                                          'F' => [1, 2], 'J' => [3, 4], '.' => Int[])
-used(frompos) = [3, 4, 1, 2][frompos]
+
+function floodfill!(m::Matrix, x, y, xdim, ydim)
+    for i in max(1, x-1):min(x+1, xdim), j in max(1, y-1):min(y+1, ydim)
+        if m[i, j] == '.'
+            m[i, j] = ' '
+            floodfill!(m, i, j, xdim, ydim)
+        end
+    end
+end
 
 function day10()
     part = [0, 0]
@@ -335,7 +343,7 @@ function day10()
     nvisited = 0
     mat[x, y] = '7'
     while true
-        move = first(setdiff(charmove[mat[x, y]], used(move)))
+        move = first(setdiff(charmove[mat[x, y]], [3, 4, 1, 2][move]))
         x, y = [x, y] .+ moves[move]
         nvisited += 1
         if haskey(visited, [x, y])
@@ -344,7 +352,7 @@ function day10()
             visited[[x, y]] = nvisited
         end
     end
-    part[1] = nvisited ÷ 2
+    part[1] = (nvisited + 1) ÷ 2
 
     graph = fill('.', 280, 282)
     for (x, y) in keys(visited)
@@ -357,26 +365,14 @@ function day10()
             graph[2x, 2y-1] = '|'
         end
     end
-
-    locations = Vector{Int}[]
     for x in 1:280, y in [1, 282]
-        graph[x, y] == '.' && push!(locations, [x, y])
-    end
-    while !isempty(locations)
-        newlocations = empty(locations)
-        for (x, y) in locations
-            for i in max(1, x-1):min(x+1, 280), j in max(1, y-1):min(y+1, 282)
-                if graph[i, j] == '.'
-                    graph[i,j] = ' '
-                    push!(newlocations, [i, j])
-                end
-            end
-        end
-        locations = newlocations
+        graph[x, y] == '.' && floodfill!(graph, x, y, 280, 282)
     end
     part[2] = count(==('.'), graph[1:2:280, 1:2:281])
+
     return part
 end
 
-@show day10()
+@show day10() # (7173, 291),
+
 
