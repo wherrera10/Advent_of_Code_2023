cc4faa0dc4f01c19785ffc0896caa86f49ce29fd
Bill <wherrera10@users.noreply.github.com>
2023-12-13 00:27:35 -1000

    Update aoc2023.jl

--- src/aoc2023.jl
+++ src/aoc2023.jl
@@ -401,4 +401,56 @@ xpan(a, b, empties, xpan) = count(i -> i ∈ empties, a < b ? (a:b-1) : (b:a-1))
 
 
 
+function day13()
+    part = [0, 0]
+
+    patterns = split(read("day13.txt", String), "\n\n")
+    mats = Matrix{Bool}[]
+    for p in patterns
+        rows = [[i =='#' for i in a] for a in split(p)]
+        m = reduce(vcat, map(a -> a', rows))
+        push!(mats, m)
+        rdelta, cdelta = findmirror(m)
+        part[1] += cdelta + 100 * rdelta
+    end
+
+    for m in mats
+        oldr, oldc = findmirror(m)
+        for i in eachindex(m)
+            m[i] = !m[i]
+            rdelta, cdelta = findmirror(m, oldr, oldc)
+            m[i] = !m[i]
+            if rdelta > 0 || cdelta > 0
+                part[2] += cdelta + 100 * rdelta
+                break
+            end
+        end
+    end
+    return part
+end
+
+function findmirror(m, forbidrow = 0, forbidcol = 0)
+    nrows, ncols = size(m)
+    rdelta, cdelta = 0, 0
+    for i in 1:nrows-1
+        i == forbidrow && continue
+        d = min(i, nrows - i)
+        if m[i-d+1:i, :] == reverse(m[i+1:i+d, :], dims=1)
+            rdelta = i
+            break
+        end
+    end
+    for i in 1:ncols-1
+        i == forbidcol && continue
+        d = min(i, ncols - i)
+        if m[:, i-d+1:i] == reverse(m[:, i+1:i+d], dims=2)
+            cdelta = i
+            break
+        end
+    end
+    return rdelta, cdelta
+end
+
+day13()
+
 
