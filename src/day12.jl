0669aa0c28740dfd162fe79850800c51e3696b35
Bill <wherrera10@users.noreply.github.com>
2023-12-18 10:46:24 -1000

    Add files via upload

--- src/day12.jl
+++ src/day12.jl
@@ -1,63 +1,63 @@
-using Memoize
-
-struct Defects
-    pat::Vector{UInt8}
-    grp::Vector{Int}
-end
-
 function day12()
     part = [0, 0]
-    defectlist = Defects[]
-    for s in filter(!isempty, readlines("day12.txt"))
-        p, x = split(s)
-        pat = [c == '.' ? 0x0 : c == '#' ? 0x1 : 0x2 for c in p]
-        grp = [parse(Int, string(s)) for s in split(x, ",")]
-        push!(defectlist, Defects(pat, grp))
-    end
+    for line in readlines("day12.txt")
+        empty!(defectcache)
+        pattern, grouptxt = split(line)
+        groups = [parse(Int, x) for x in split(grouptxt, ",")]
+        part[1] += countdefects(pattern, 1, groups, 1)
 
-    worklist = deepcopy(defectlist)
-
-    for d in worklist
-        n = processdefects(d)
-        part[1] += n
-    end
-    @show part[1]
-
-    part2start = time()
-    worklist2 = [Defects(repeat(vcat(d.pat, [0x2]), 5)[begin:end-1], repeat(d.grp, 5)) for d in worklist]
-    println("Part 2 worklist has $(length(worklist2)) items.")
-    slock = ReentrantLock()
-    @Threads.threads for d in worklist2
-        n = processdefects(d)
-        elap = time() - part2start
-        println("results $n elapsed $(Int(round(elap))) seconds.")
-        lock(slock)
-        part[2] += n
-        unlock(slock)
+        empty!(defectcache)
+        pattern2 = join([pattern for _ in 1:5], "?")
+        groups2 = repeat(groups, 5)
+        part[2] += countdefects(pattern2, 1, groups2, 1)
     end
 
     return part
 end
 
+const defectcache = Dict{Pair{Int, Int}, Int}()
 
-""" 0x0 is .  0x1 is #  0x2 is ? """
-@memoize function processdefects(d::Defects)
-    pat, grp, nfound = d.pat, d.grp, 0
-    #isempty(pat) && return isempty(grp) ? 1 : 0
-    isempty(grp) && return (0x1 in pat) ? 0 : 1
+function countdefects(pat, pidx, grp, gidx)
     plen, glen = length(pat), length(grp)
-    plen < sum(grp) + glen - 1 && return 0
-    p, g = first(pat), first(grp)
-    if 0x0 ∉ pat[begin:g] # . found in part of what we want to be run of # or ?
-        if plen == g || pat[g+1] != 0x1
-            nfound += processdefects(Defects(pat[g+2:end], grp[begin+1:end]))
+    while pidx <= plen && pat[pidx] == '.' # skip past '.'
+        pidx += 1
+    end
+    haskey(defectcache, pidx => gidx) && return defectcache[pidx => gidx]
+    if gidx == glen + 1 # done with groups
+        n = all(c -> c ∈ ['.', '?'], pat[pidx:end]) # no more defects needed
+        defectcache[pidx => gidx] = Int(n)
+        return Int(n)
+    end
+    pidx > plen && return 0 # done with pattern, but still have group to fulfill
+    g, i = grp[gidx], pidx
+    gotmatch = false
+    while g > 0 && i < length(pat) + 1 && pat[i] in ['?', '#']
+        gotmatch |= pat[i] == '#'
+        i += 1
+        g -= 1
+    end
+    g > 0 && (gotmatch || i > length(pat)) && return 0
+    g > 0 && i <= length(pat) && return countdefects(pat, i + 1, grp, gidx)
+    matchcount = 0
+    if i == length(pat) + 1 || pat[i] != '#'
+        matchcount += countdefects(pat, i + 1, grp, gidx + 1)
+    end
+    j = pidx
+    while i <= length(pat) && pat[j] == '?' && pat[i] in ['#', '?']
+        gotmatch |= pat[i] == '#'
+        j += 1
+        i += 1
+        if i > length(pat)  || pat[i] != '#'
+            matchcount += countdefects(pat, i + 1, grp, gidx + 1)
         end
     end
-    if p != 0x1 # not a #
-        nfound += processdefects(Defects(pat[begin+1:end], grp))
+    if !gotmatch && i <= length(pat)
+        matchcount += countdefects(pat, i + 1, grp, gidx)
     end
-    return nfound
+    defectcache[pidx => gidx] = matchcount
+    return matchcount
 end
 
+@show day12()
 
-day12() # 7361,
+
