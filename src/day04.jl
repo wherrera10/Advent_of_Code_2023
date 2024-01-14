b777ae0249835f0d594dbc1a3ef16413c922d0c1
Bill <wherrera10@users.noreply.github.com>
2024-01-13 22:15:34 -1000

    Update day04.jl

--- src/day04.jl
+++ src/day04.jl
@@ -1,28 +1,20 @@
-function day04()
-    lines = strip.(filter(!isempty, readlines("day04.txt")))
-    wins = Vector{Int}[]
-    allcards = collect(1:length(lines))
-    part = [0, 0]
+using Memoize
 
-    for (cardnum, line) in enumerate(lines)
-        wintxt, gottxt = strip.(split(line, r"\:|\|"))[2:3]
-        matches = [parse(Int, s) for s in split(wintxt, r"\s+") ] ∩ [parse(Int, s) for s in split(gottxt, r"\s+")]
-        if isempty(matches)
-            push!(wins, Int[])
-        else
-            push!(wins, filter(<=(length(lines)), cardnum+1:cardnum+length(matches)))
-            part[1] += 2^(length(matches)-1)
+const scores4 = Dict{Int, Int}()
+@memoize scoring(i) = scores4[i] < 1 ? 0 : scores4[i] + sum(scoring(j) for j in i+1:i+scores4[i])
+
+function day04()
+    part = [0, 0]
+    lines = readlines("day04.txt")
+    for (i, line) in enumerate(lines)
+        winners, have = [parse.(Int, split(s)) for s in split(line, r"\:|\|")[begin+1:end]]
+        scores4[i] = length(intersect(winners, have))
+        if scores4[i] > 0
+            part[1] += exp2(scores4[i] - 1)
         end
     end
-
-    i = 0
-    while i < length(allcards)
-        i += 1
-        append!(allcards, wins[allcards[i]])
-    end
-    part[2] = length(allcards)
-
-    @show part[1], part[2]
+    part[2] = sum(scoring(i) + 1 for i in eachindex(lines))
+    return part
 end
 
 @time day04()
