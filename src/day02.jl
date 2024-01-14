cafc77f53d55f2f51ce6aac027cefc9d84a6924d
Bill <wherrera10@users.noreply.github.com>
2024-01-13 21:21:36 -1000

    Update day02.jl

--- src/day02.jl
+++ src/day02.jl
@@ -1,37 +1,27 @@
+const day2game = [Int16[], Int16[], Int16[]]
+
 function day02()
-    lines = filter(!isempty, strip.(readlines("day02.txt")))
-    part1given = Dict("red" => 12, "green" => 13, "blue" => 14)
-    part1possibles = Int[]
-    powers = Int[]
-    part = [0, 0]
-
-    for (gamenum, line) in enumerate(lines)
-        gameshowings = split(line, r":|;")[2:end]
-        pairvec = Pair{String, Int}[]
-        for showing in gameshowings
-            pairs = strip.(split(showing, ","))
-            d = Dict{String, Int}()
-            for pair in pairs
-                num, colr = strip.(split(pair, r"\s+"))
-                push!(pairvec, colr => parse(Int, num))
-            end
-        end
-        maxshow = Dict{String, Int}()
-        for (clr, cnt) in pairvec
-            maxshow[clr] = max(get!(maxshow, clr, 0), cnt)
-        end
-        push!(powers, prod(values(maxshow)))
-        ispossible = true
-        for (colr, cnt) in maxshow
-            if cnt > part1given[colr]
-                ispossible = false
-            end
-        end
-        ispossible && push!(part1possibles, gamenum)
-    end
-
-    part .= sum(part1possibles), sum(powers)
-    @show(part)
+	part = [0, 0]
+	part1vals = [12, 13, 14] # red, green, blue
+	games = Vector{Vector{Int}}[]
+	for (i, line) in enumerate(readlines("day02.txt"))
+		push!(games, deepcopy(day2game))
+		numcols = split(line, r"\:|\;")[begin+1:end]
+		for nc in numcols
+			foreach(i -> push!(games[end][i], 0), 1:3)
+			nc = split(nc, ",")
+			for s in nc
+				n, c = strip.(split(s))
+				i = c[1] == 'r' ? 1 : c[1] == 'g' ? 2 : 3
+				games[end][i][end] = parse(Int, n)
+			end
+		end
+	end
+	part[1] = sum(gamenum * all(!any(game[i][j] > part1vals[i] for i in 1:3, 
+	   j in eachindex(game[begin]))) for (gamenum, game) in enumerate(games))
+	part[2] = sum(prod(maximum(game[i][j] for j in eachindex(game[begin]))
+	   for i in 1:3) for game in games)
+	return part
 end
 
 @time day02()
