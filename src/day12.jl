1508194702a827017cf5e18684efb29ec9342b11
Bill <wherrera10@users.noreply.github.com>
2024-01-28 13:32:22 -1000

    Update day12.jl

--- src/day12.jl
+++ src/day12.jl
@@ -1,61 +1,65 @@
+struct Spring12
+    pattern::Vector{Char}
+    groups::Vector{Int}
+end
+
 function day12()
     part = [0, 0]
+    springs = Spring12[]
     for line in readlines("day12.txt")
-        empty!(defectcache)
-        pattern, grouptxt = split(line)
-        groups = [parse(Int, x) for x in split(grouptxt, ",")]
-        part[1] += countdefects(pattern, 1, groups, 1)
-
-        empty!(defectcache)
-        pattern2 = join([pattern for _ in 1:5], "?")
-        groups2 = repeat(groups, 5)
-        part[2] += countdefects(pattern2, 1, groups2, 1)
+        pat, grouptxt = split(line)
+        push!(springs, Spring12(collect(pat), [parse(Int, x) for x in split(grouptxt, ",")]))
     end
+
+    part[1] = sum(solve12(s, 1) for s in springs)
+    part[2] = sum(solve12(s, 5) for s in springs)
 
     return part
 end
 
-const defectcache = Dict{Pair{Int, Int}, Int}()
+""" method as in https://github.com/maneatingape/advent-of-code-rust/blob/main/src/year2023/day12.rs """
+function solve12(spring, repeats)
+    springsum = 0
+    broken = zeros(Int, 200)
+    table = zeros(Int, 10_000)
 
-function countdefects(pat, pidx, grp, gidx)
-    plen, glen = length(pat), length(grp)
-    while pidx <= plen && pat[pidx] == '.' # skip past '.'
-        pidx += 1
+    pattern = repeat(vcat(spring.pattern, '?'), repeats)
+    pattern[end] = '.'
+    groups = repeat(spring.groups, repeats)
+    for (i, b) in enumerate(pattern)
+        b != '.' && (springsum += 1)
+        broken[i + 1] = springsum
     end
-    haskey(defectcache, pidx => gidx) && return defectcache[pidx => gidx]
-    if gidx == glen + 1 # done with groups
-        n = all(c -> c ∈ ['.', '?'], pat[pidx:end]) # no more defects needed
-        defectcache[pidx => gidx] = Int(n)
-        return Int(n)
-    end
-    pidx > plen && return 0 # done with pattern, but still have group to fulfill
-    g, i = grp[gidx], pidx
-    lastwashash = false
-    while g > 0 && i <= length(pat) && pat[i] ∈ ['?', '#']
-        lastwashash |= pat[i] == '#'
-        i += 1
-        g -= 1
-    end
-    g > 0 && (lastwashash || i > length(pat)) && return 0 # group donne and no spacer yet
-    g > 0 && i <= length(pat) && return countdefects(pat, i + 1, grp, gidx)
-    matchcount = 0
-    if i > length(pat) || pat[i] != '#' # spacer found, do next group
-        matchcount += countdefects(pat, i + 1, grp, gidx + 1)
-    end
-    j = pidx
-    while i <= length(pat) && pat[j] == '?' && pat[i] ∈ ['#', '?']
-        lastwashash |= pat[i] == '#'
-        j += 1
-        i += 1
-        if i > length(pat) || pat[i] != '#'
-            matchcount += countdefects(pat, i + 1, grp, gidx + 1) # . or ? so do next group
+    wiggle = length(pattern) - sum(groups) - length(groups) + 1
+    siz = first(groups)
+    springsum = 0
+    valid = true
+    for i in 1:wiggle
+        if pattern[i + siz] == '#'
+            springsum = 0
+        elseif valid && broken[i + siz] - broken[i] == siz
+            springsum += 1
         end
+        table[i + siz] = springsum
+        valid &= pattern[i] != '#'
     end
-    if !lastwashash && i <= length(pat) # can also count matches from one further along pat
-        matchcount += countdefects(pat, i + 1, grp, gidx)
+    start = siz + 1
+    for (row, gsize) in enumerate(@view groups[2:end])
+        previous = (row - 1) * length(pattern)
+        current = row * length(pattern)
+        springsum = 0
+        for i in start:start+wiggle
+            if pattern[i + gsize] == '#'
+                springsum = 0
+            elseif table[previous + i - 1] > 0 &&
+               pattern[i - 1] != '#' && broken[i + gsize] - broken[i] == gsize
+                springsum += table[previous + i - 1]
+            end
+            table[current + i + gsize] = springsum
+        end
+        start += gsize + 1
     end
-    defectcache[pidx => gidx] = matchcount
-    return matchcount
+    return springsum;
 end
 
 @show day12()
