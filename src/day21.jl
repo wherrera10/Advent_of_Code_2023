780d1df921f5fd05859ec5d4a3da350895d0a4e9
Bill <wherrera10@users.noreply.github.com>
2024-01-29 09:01:55 -1000

    Update day21.jl

--- src/day21.jl
+++ src/day21.jl
@@ -1,63 +1,62 @@
-const dirs = CartesianIndex.([(0, 1), (1, 0), (0, -1), (-1, 0)]) # E S W N
-
 function day21()
-	part = [0, 0]
-	grid = Char.(stack(Iterators.map(collect, eachline("day21.txt")), dims = 1))
-	nrows, ncols = size(grid)
-	start = first(findall(==('S'), grid))
+    part = [0, 0]
+    grid = Char.(stack(Iterators.map(collect, eachline("day21.txt")); dims=1))
+    nrows, ncols = size(grid)
+    start = first(findall(==('S'), grid))
+    corners = CartesianIndex.([(1, 1), (nrows, 1), (1, ncols), (nrows, ncols)])
+    even_inner, even_outer, odd_inner, odd_outer = bfs21(grid, [start], start, 130)
+    part[1] = even_inner
 
-	@show nrows, ncols, start
-	grid[start] = '.'
-	paths = [start]
-	for _ in 1:nrows÷2-1
-		newpaths = empty(paths)
-		for p in paths
-			append!(newpaths, neighbors(p, grid, nrows, ncols))
-		end
-		unique!(newpaths)
-		empty!(paths)
-		append!(paths, newpaths)
-	end
-	part[1] = length(unique(paths))
-	@show part[1]
+    even_full = even_inner + even_outer
+    odd_full = odd_inner + odd_outer
+    remove_corners = odd_outer
+    add_corners = bfs21(grid, corners, start, 64)[begin]
+    
+    n = 202300
+    firststep = n * n * even_full
+    second = (n + 1) * (n + 1) * odd_full
+    third = n * add_corners
+    fourth = (n + 1) * remove_corners
+    part[2] = firststep + second + third - fourth
 
-	biggrid = repeat(grid, outer = (5, 5))
-	start2 = CartesianIndex(nrows * 2 + nrows ÷ 2 + 1, ncols * 2 + ncols ÷ 2 + 1)
-	biggrid[start2] = 'O'
-	foreach(_ -> floodfill!(biggrid), 1:nrows÷2)
-    y1 = count(==('O'), biggrid)
-	foreach(_ -> floodfill!(biggrid), 1:nrows)
-    y2 = count(==('O'), biggrid)
-	foreach(_ -> floodfill!(biggrid), 1:nrows)
-    y3 = count(==('O'), biggrid)
-	a = (y3 - 2 * y2 + y1) / big"2"
-	b = (4 * y2 - 3 * y1 - y3) / big"2"
-	c = BigFloat(y1)
-	t = (26501365 - nrows ÷ 2) ÷ nrows
-	part[2] = BigInt(round(a * t^2 + b * t + c))
-
-	return part
+    return part
 end
 
-function neighbors(c, grid, nrows, ncols)
-	neigh = CartesianIndex[]
-	for d in dirs
-		newc = d + c
-		if 1 <= newc[1] <= nrows && 1 <= newc[2] <= ncols && grid[newc] == '.'
-			push!(neigh, newc)
-		end
-	end
-	return neigh
-end
-
-function floodfill!(grid)
-	current = findall(==('O'), grid)
-	grid[current] .= '.'
-	for c in current, d in dirs
-		if grid[c+d] != '#'
-			grid[c+d] = 'O'
-		end
-	end
+function bfs21(grid, starts, center, limit)
+    manhattan(c1, c2) = abs(c1[1] - c2[1]) + abs(c1[2] - c2[2])
+    dirs = CartesianIndex.([(0, 1), (1, 0), (0, -1), (-1, 0)]) # E S W N
+    nrows, ncols = size(grid)
+    halfway = (nrows + ncols) ÷ 4
+    mat = deepcopy(grid)
+    todo = Tuple{CartesianIndex, Int}[]
+    even_inner, even_outer, odd_inner, odd_outer = 0, 0, 0, 0
+    for c in starts
+        mat[c] = '#'
+        push!(todo, (c, 0))
+    end
+    while !isempty(todo)
+        position, cost = popfirst!(todo)
+        if isodd(cost)
+            if manhattan(position, center) <= halfway
+                odd_inner += 1
+            else
+                odd_outer += 1
+            end
+        elseif cost < halfway
+            even_inner += 1
+        else
+            even_outer += 1
+        end
+        if cost < limit
+            for nxt in position .+ dirs
+                if 0 < nxt[1] <= nrows && 0 < nxt[2] <= ncols && mat[nxt] != '#'
+                    mat[nxt] = '#'
+                    push!(todo, (nxt, cost + 1))
+                end
+            end
+        end
+    end
+    return even_inner, even_outer, odd_inner, odd_outer
 end
 
 @show day21()
